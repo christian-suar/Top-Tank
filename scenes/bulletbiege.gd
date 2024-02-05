e3b8b8f80c0a0614ab043943f1c36ddcfe319e5f
christiansuar <148103464+christiansuar@users.noreply.github.com>
2024-02-05 11:12:34 -0600

    tilemap n bullet
    made tilemap prettier and began work on a bullet

---
+++ scenes/bulletbiege.gd
@@ -0,0 +1,6 @@
+extends CharacterBody2D
+
+var damage = 2
+var speed = 50
+
+
--- scenes/player_tank.gd
+++ scenes/player_tank.gd
@@ -3,8 +3,10 @@ extends CharacterBody2D
 
 const speed = 250
 const rotation_speed = 1.2
-
-
+var can_fire = true
+@onready var shoot_spot = $Marker2D
+@onready var bullet_scene = preload("res://scenes/bulletbiege.tscn")
+@onready var tileSet = $"../TileMap"
 
 func _physics_process(delta):
 	var move_input = Input.get_axis("ui_down", "ui_up")
@@ -12,4 +14,14 @@ func _physics_process(delta):
 	velocity = transform.x * move_input * speed
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
-	
+	if Input.is_action_just_pressed("leftClick") and can_fire:
+		var bullet = bullet_scene
+		bullet.position = shoot_spot.position
+		print("pos done")
+		bullet.velocity = Vector2(128,0)
+		
+		tileSet.add_child(bullet)
+		can_fire = false
+		await get_tree().create_timer(1).timeout
+		can_fire = true
+

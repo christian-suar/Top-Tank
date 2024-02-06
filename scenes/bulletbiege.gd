b964a2f65e0a6595a7b0db968897686217b49588
christiansuar <148103464+christiansuar@users.noreply.github.com>
2024-02-06 11:09:54 -0600

    worked on bullet
    will figure out how to do the environment next

--- scenes/bulletbiege.gd
+++ scenes/bulletbiege.gd
@@ -3,4 +3,6 @@ extends CharacterBody2D
 var damage = 2
 var speed = 50
 
+func _process(delta):
+	move_and_slide()
 
--- scenes/player_tank.gd
+++ scenes/player_tank.gd
@@ -6,7 +6,7 @@ const rotation_speed = 1.2
 var can_fire = true
 @onready var shoot_spot = $Marker2D
 @onready var bullet_scene = preload("res://scenes/bulletbiege.tscn")
-@onready var tileSet = $"../TileMap"
+@onready var tileSet = $".."
 
 func _physics_process(delta):
 	var move_input = Input.get_axis("ui_down", "ui_up")
@@ -14,13 +14,19 @@ func _physics_process(delta):
 	velocity = transform.x * move_input * speed
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
+	
+	
+	
+	
 	if Input.is_action_just_pressed("leftClick") and can_fire:
-		var bullet = bullet_scene
+
+		var bullet = bullet_scene.instantiate()
 		bullet.position = shoot_spot.position
 		print("pos done")
 		bullet.velocity = Vector2(128,0)
-		
-		tileSet.add_child(bullet)
+		bullet.rotation = rotation
+		$".".add_child(bullet)
+		print("added to tile")
 		can_fire = false
 		await get_tree().create_timer(1).timeout
 		can_fire = true
